863862c6ad2708851ce4120c4363e501a72aaef9
cater20 <onyangoketta@gmail.com>
2022-06-22 06:59:00 +0300

    add

---
+++ index.js
@@ -0,0 +1,16 @@
+const input = document.getElementsByClassId('call');
+  
+function click1() {
+  alert("CALL : +254792149427");
+}
+
+input.addEventListener('click', click1);
+
+//onclick on the email
+const emailIcon = document.getElementById('email');
+  
+function onClickAlert() {
+  alert("EMAIL : chrisphineketta@gmail.com");
+}
+
+emailIcon.addEventListener('click', onClickAlert);
